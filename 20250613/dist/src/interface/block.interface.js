eccb5396fc0ff8989f85409ca270f27828075337
Mr-Binod <blackorion3@gmail.com>
2025-07-09 17:54:43 +0900

    20250709

--- 20250613/dist/src/interface/block.interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// data : IBockHeader {
-// }
-// data : string 
-// data = '1'
-// string number
-// {name : }
--- 20250613/src/interface/wallet.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface IWallet {
-    account : string; // 공개키로 만든 주소의 값 내 지갑의 계좌번호 , 개인키는 계좌의 비밀번호
-    privateKey : string;
-    publicKey : string;
-    balance : number; // 잔액 -> 사용자에게 제공하기위한 인터페이스 -> UTXO (자산의 내용 {account : 10232, balance : 2323})
-}
